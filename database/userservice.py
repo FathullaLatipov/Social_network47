1e2f3f6a66deebe074e19c5e300dd8fe61736553
FathullaLatipov <fathullaxonlatipov@gmail.com>
2024-03-12 20:40:19 +0500

    done like and more functions

--- database/userservice.py
+++ database/userservice.py
@@ -32,12 +32,73 @@ def register_user_db(name, surname, phone_number, city, password):
         db.commit()
         return f'Успешно зарегистрированы {new_user.user_id}'
 
-# ДЗ
-# Логин def login_user_db(phone_number, password)
-# Delete user in db def delete_user_db(user_id)
+
+# Логин
+def login_user_db(phone_number, password):
+    db = next(get_db())
+    login = db.query(User).filter_by(phone_number=phone_number, password=password).first()
+    if login:
+        return f'Вход выполнен успешно для пользователя {login.user_id}'
+    else:
+        return 'Неверный номер телефона или пароль'
+
+
+#  Удаления пользователя
+def delete_user_db(user_id):
+    db = next(get_db())
+    user = db.query(User).filter_by(user_id=user_id).first()
+    if user:
+        db.delete(user)
+        db.commit()
+        return f'Пользователь с ID {user_id} удален'
+    else:
+        return 'Пользователь не найден'
+
+
+# Изменения данных пользователя
+def edit_user_info_db(user_id, edit_info, new_info):
+    db = next(get_db())
+
+    exact_user = get_exact_user_db(user_id)  # 3
+
+    if exact_user:
+        if edit_info == 'name':
+            exact_user.name = new_info
+        elif edit_info == 'surname':
+            exact_user.surname = new_info
+
+        db.commit()
+        return 'Данные успешно изменены!'
+    else:
+        return 'Пользователь не найден(('
+
 
 # Мы будем делать
-# Изменения информации о пользователее
 # Добавить фото профиля
-# Удаления фото профиля и начнем postservice.py
+def upload_profile_photo_db(user_id, photo_path):
+    db = next(get_db())
 
+    exact_user = get_exact_user_db(user_id)
+
+    if exact_user:
+        exact_user.profile_photo = photo_path
+        db.commit()
+
+        return 'Успешно!'
+    else:
+        return 'Пользователь не найден(('
+
+
+# Удаления фото профиля и начнем postservice.py
+def delete_profile_photo_db(user_id):
+    db = next(get_db())
+
+    exact_user = get_exact_user_db(user_id)
+
+    if exact_user:
+        exact_user.profile_photo = 'None'
+        db.commit()
+
+        return 'Фото профиля удален'
+    else:
+        return 'Пользователь не найден(('
