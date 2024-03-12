5efe3cf3e6842ed243aa3d330825360020035b22
FathullaLatipov <fathullaxonlatipov@gmail.com>
2024-03-12 21:35:31 +0500

    done services

--- database/postservice.py
+++ database/postservice.py
@@ -63,7 +63,7 @@ def add_like_post_db(post_id):
     db = next(get_db())
     exact_post = db.query(UserPost).filter_by(post_id=post_id).first()
     if exact_post:
-        exact_post.like += 1
+        exact_post.likes += 1
         db.commit()
         return '+1 Like'
     else:
@@ -74,8 +74,47 @@ def unlike_post_db(post_id):
     db = next(get_db())
     exact_post = db.query(UserPost).filter_by(post_id=post_id).first()
     if exact_post:
-        exact_post.like -= 1
+        exact_post.likes -= 1
         db.commit()
         return '-1 Like'
     else:
         return 'Post not found'
+
+
+# Загрузить фотографии к определенному посту
+def upload_post_photo_db(post_id, photo_path):
+    db = next(get_db())
+
+    new_photo = PostPhoto(post_id=post_id, photo_path=photo_path)
+
+    if new_photo:
+        db.add(new_photo)
+        db.commit()
+
+        return 'Фото добавлен!'
+    else:
+        return 'Нету поста Брат('
+
+
+# Удалить фотографию определенного поста
+def delete_post_photo_db(post_id):
+    db = next(get_db())
+
+    new_photo = db.query(PostPhoto).filter_by(post_id=post_id).first()
+
+    if new_photo:
+        # Если не будет работать то Капзда Дильмуроду
+        db.delete(new_photo.photo_path)
+        db.commit()
+
+        return 'Фото добавлен!'
+    else:
+        return 'Нету поста Брат('
+
+
+# Запрос на получении всех фотографий
+def all_photos_db():
+    db = next(get_db())
+
+    photos = db.query(PostPhoto).all()
+    return photos
